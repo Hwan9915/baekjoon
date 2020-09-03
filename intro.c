819139e72b017bde74481ffd9d5f962c949067d9
Hwan9915 <cubic0915@naver.com>
2020-09-03 22:57:49 +0900

    commit

---
+++ intro.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a[5],num=1,k;
+    for(int i=0;i<5;i++) scanf("%d",&a[i]);
+    while(1)
+    {
+        k=0;
+        for(int j=0;j<5;j++) if(num%a[j]==0) k++;
+        if(k>2) break;
+        num++;
+    }
+    printf("%d",num);
+
+    return 0;
+}
